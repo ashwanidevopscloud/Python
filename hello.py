8ffa781c0a529b9c2f303056c57f9799d1cb0755
Ankireddypalleashwani <101695351+Ankireddypalleashwani@users.noreply.github.com>
2024-12-14 22:03:14 +0530

    adding files

---
+++ hello.py
@@ -0,0 +1,77 @@
+# 10 * 20
+# (10 * 20 )* 40-30/2 
+print("Hello World")
+
+# variables
+# 1.Integer
+# 2.Float / double
+# 3.strings
+# 4.Boolean
+# 5.list
+# 6.Tuples
+# 7.Dictionary
+# 8.sets
+
+# List and tuples both are not same
+
+res = 10+20
+print("Result:" , res)
+print(10+20)
+print("Memory Address: ",id(res))
+#they are methods specific to that data type
+print("Data Type:", type(res))
+
+# this is line comment
+"""
+this is block comment
+"""
+
+
+a=10.23
+b=20.34
+res=a+b
+print("Result:", res, "Type:", type(res))
+
+sample_str="this is a string"
+# print(sample_str, type(sample_str))
+# print(dir(sample_str))
+"""
+capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 
+'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 
+'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 
+'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle',
+ 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans',
+  'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind',
+   'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split',
+    'splitlines', 'startswith', 'strip', 'swapcase', 'title',
+     'translate', 'upper', 'zfill'
+"""
+# print(sample_str.capitalize())
+# print(sample_str.casefold())
+# print(sample_str.center())
+
+# txt = "Hello , And Welcome To My World"
+# x = txt.casefold()
+# print(x)
+
+# print(txt.swapcase())
+
+a=True
+b=False
+
+print(a and b) # false
+print( a or b) # True
+
+a=2
+b=4
+
+print(a+b) # add
+print(a-b) # sub
+print(a*b) # multi
+print(a/b) # division
+print(b%a) # module division: return remainder
+print(a//b) # integer division
+print(a ** b) # expontentiational
+
+
+
